073ca3f87ddaa9b24356ebfec7c0068699af34d9
dtesters <144807230+dtesters@users.noreply.github.com>
2023-09-16 18:26:08 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const card = document.querySelector('.card');
+
+// Change card background color on hover
+card.addEventListener('mouseenter', () => {
+    card.style.backgroundColor = '#FFA500'; /* Orange */
+});
+
+card.addEventListener('mouseleave', () => {
+    card.style.backgroundColor = '#333'; /* Initial card background color */
+});
+
+// Change button background color on hover
+const buttons = document.querySelectorAll('.animated-button');
+buttons.forEach((button) => {
+    button.addEventListener('mouseenter', () => {
+        button.style.backgroundColor = '#FF1493'; /* Pink */
+    });
+
+    button.addEventListener('mouseleave', () => {
+        button.style.backgroundColor = '#0000FF'; /* Blue */
+    });
+});
