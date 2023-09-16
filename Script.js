bc98d4ef54c24eef3a41abac0e568d2b6c9f7f2c
dtesters <144807230+dtesters@users.noreply.github.com>
2023-09-16 19:15:38 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,36 @@
-const darkModeButton = document.getElementById("dark-mode-button");
-const body = document.body;
+// JavaScript code for handling RGB animations
+function rgbAnimation(element, property, speed) {
+    let currentColor = [255, 0, 0]; // Initial color (red)
+    const targetColor = [0, 255, 0]; // Target color (green)
 
-darkModeButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
+    const interval = setInterval(function () {
+        let done = true;
+        for (let i = 0; i < 3; i++) {
+            if (currentColor[i] !== targetColor[i]) {
+                done = false;
+                currentColor[i] += currentColor[i] < targetColor[i] ? 1 : -1;
+            }
+        }
+
+        if (done) {
+            clearInterval(interval);
+        }
+
+        element.style[property] = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`;
+    }, speed);
+}
+
+// Apply RGB animations to elements
+const title = document.querySelector('.title');
+const description = document.querySelector('.description');
+const buttons = document.querySelectorAll('.button');
+const card = document.querySelector('.card');
+
+rgbAnimation(title, 'color', 100); // Title text color
+rgbAnimation(description, 'color', 100); // Description text color
+
+buttons.forEach(function (button) {
+    rgbAnimation(button, 'backgroundColor', 200); // Button background color
 });
+
+rgbAnimation(card, 'backgroundColor', 200); // Card background color
